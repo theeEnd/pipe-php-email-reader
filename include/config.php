fc18d820a4338bd58fc281bd7467f20f14feae98
Ivan Ivanov <iki03@mail.bg>
2016-02-15 23:06:44 +0200

    2016-02-15 v1

---
+++ include/config.php
@@ -0,0 +1,37 @@
+<?php
+
+/* 
+ * @desc configuration
+ */
+
+/** 
+ * What's the max # of seconds to try to process
+ */
+define('MAX_TIME_LIMIT', 600);
+
+/**
+ * Configure your MySQL database connection here
+ */
+
+/** 
+ * MySQL hostname
+ */
+define('DB_HOST', '******');
+
+/**
+ * The name of the database for WordPress
+ */
+define('DB_NAME', '******');
+
+/**
+ * MySQL database username
+ */
+define('DB_USER', '******');
+
+/**
+ * MySQL database password
+ */
+define('DB_PASSWORD', '******');
+
+set_time_limit(MAX_TIME_LIMIT);
+ini_set('max_execution_time', MAX_TIME_LIMIT);
